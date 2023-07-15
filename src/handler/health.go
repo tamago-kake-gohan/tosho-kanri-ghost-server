9eb227b65d7014252b6db35ad1afdd9273c37cbc
xpadev <xpadev@gmail.com>
2023-07-15 15:16:55 +0900

    fix: move response type to handler

--- src/handler/health.go
+++ src/handler/health.go
@@ -4,9 +4,11 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-
-	"tamago-kake-gohan.github.io/tosho-kanri-ghost/src/model"
 )
+
+type HealthResponse struct {
+	Message string `json:"message"`
+}
 
 type HealthHandler struct{}
 
@@ -16,7 +18,7 @@ func NewHealthHandler() *HealthHandler {
 
 func (h *HealthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	data := model.HealthResponse{}
+	data := HealthResponse{}
 	data.Message = "OK"
 	err := json.NewEncoder(w).Encode(data)
 	if nil != err {
--- src/model/health.go
+++
@@ -1,5 +0,0 @@
-package model
-
-type HealthResponse struct {
-	Message string `json:"message"`
-}
