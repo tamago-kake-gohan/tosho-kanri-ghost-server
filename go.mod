71eb8e1bee75acdd0a9d7ee42fe1af1c3baa92dc
xpa <xpadev@gmail.com>
2023-07-15 12:28:13 +0900

    feat: env loader

---
+++ go.mod
@@ -0,0 +1,7 @@
+module tamago-kake-gohan.github.io/tosho-kanri-ghost
+
+go 1.20
+
+require github.com/joho/godotenv v1.5.1
+
+require github.com/go-sql-driver/mysql v1.7.1
--- src/main.go
+++ src/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"os"
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
+	loadEnv()
 	fmt.Println("test")
+}
+
+func loadEnv() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		fmt.Printf("読み込み出来ませんでした: %v", err)
+	}
 }
