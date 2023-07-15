f5f609421e15e1befb6e40f3fe1224bab90ec081
xpa <xpadev@gmail.com>
2023-07-16 04:16:18 +0900

    feat: /api/v1/process_return

--- src/handler/process_return.go
+++ src/handler/process_return.go
@@ -10,29 +10,28 @@ import (
 	"tamago-kake-gohan.github.io/tosho-kanri-ghost/src/utils"
 )
 
-type ProcessRequestResponse struct {
+type ProcessReturnResponse struct {
 	Message string `json:"message"`
 	Status  string `json:"status"`
 }
 
-type ProcessRequestHandler struct {
+type ProcessReturnHandler struct {
 	sess *session.Manager
 	db   *sql.DB
 }
 
-type ProcessRequestBody struct {
-	UserBookId int  `json:"user_book_id"`
-	Accept     bool `json:"accept"`
+type ProcessReturnBody struct {
+	UserBookId int `json:"user_book_id"`
 }
 
-func NewProcessRequestHandler(sess *session.Manager, db *sql.DB) *ProcessRequestHandler {
-	return &ProcessRequestHandler{
+func NewProcessReturnHandler(sess *session.Manager, db *sql.DB) *ProcessReturnHandler {
+	return &ProcessReturnHandler{
 		sess: sess,
 		db:   db,
 	}
 }
 
-func (h *ProcessRequestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h *ProcessReturnHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	sess := h.sess.SessionStart(w, r)
 	userId := sess.Get("user_id")
@@ -45,25 +44,21 @@ func (h *ProcessRequestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request
 		json.NewEncoder(w).Encode(response)
 		return
 	}
-	var body ProcessRequestBody
+	var body ProcessReturnBody
 	json.Unmarshal(utils.GetRequestBody(r), &body)
 
 	var userBook = model.UserBook{}
-	err := h.db.QueryRow("SELECT * FROM `UserLendBook` WHERE `UserId` = ? AND `Id` = ? AND `Status` = 'requested'", userId, body.UserBookId).Scan(&userBook.Id, &userBook.UserId, &userBook.BookId, &userBook.State)
+	err := h.db.QueryRow("SELECT * FROM `UserLendBook` WHERE `UserId` = ? AND `Id` = ? AND `Status` = 'accepted'", userId, body.UserBookId).Scan(&userBook.Id, &userBook.UserId, &userBook.BookId, &userBook.State)
 	if nil != err {
 		w.WriteHeader(http.StatusNotFound)
-		response := ProcessRequestResponse{}
+		response := ProcessReturnResponse{}
 		response.Message = "該当の本が見つかりませんでした"
 		response.Status = "error"
 		json.NewEncoder(w).Encode(response)
 	}
-	status := "accepted"
-	if !body.Accept {
-		status = "rejected"
-	}
-	h.db.Exec("UPDATE `UserLendBook` SET `Status` = ? WHERE `Id` = ?", status, body.UserBookId)
-	response := ProcessRequestResponse{}
-	response.Message = "リクエストを処理しました"
+	h.db.Exec("UPDATE `UserLendBook` SET `Status` = 'returned' WHERE `Id` = ?", body.UserBookId)
+	response := ProcessReturnResponse{}
+	response.Message = "返却しました"
 	response.Status = "success"
 	json.NewEncoder(w).Encode(response)
 }
--- src/router/router.go
+++ src/router/router.go
@@ -26,5 +26,6 @@ func NewRouter(db *sql.DB, session *session.Manager) *http.ServeMux {
 	mux.Handle("/api/v1/request_rental", utils.CORS(handler.NewRequestRentalHandler(session, db)))
 	mux.Handle("/api/v1/try_auth", utils.CORS(handler.NewTryAuthHandler(session)))
 	mux.Handle("/api/v1/process_request", utils.CORS(handler.NewProcessRequestHandler(session, db)))
+	mux.Handle("/api/v1/process_return", utils.CORS(handler.NewProcessReturnHandler(session, db)))
 	return mux
 }
