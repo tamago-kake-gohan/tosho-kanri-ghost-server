0b27705d3e02f910a03a2db1d3c975025bca9ea5
xpa <xpadev@gmail.com>
2023-07-16 00:51:16 +0900

    feat: cors

--- src/router/router.go
+++ src/router/router.go
@@ -6,17 +6,18 @@ import (
 
 	"github.com/astaxie/session"
 	"tamago-kake-gohan.github.io/tosho-kanri-ghost/src/handler"
+	"tamago-kake-gohan.github.io/tosho-kanri-ghost/src/utils"
 )
 
 func NewRouter(db *sql.DB, session *session.Manager) *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.Handle("/session_test", handler.NewSessionTestHandler(session))
-	mux.Handle("/health", handler.NewHealthHandler())
-	mux.Handle("/api/v1/login", handler.NewLoginHandler(session, db))
-	mux.Handle("/api/v1/register", handler.NewRegisterHandler(session, db))
-	mux.Handle("/api/v1/logout", handler.NewLogoutHandler(session))
-	mux.Handle("/api/v1/get_teams", handler.NewGetTeamsHandler(session, db))
-	mux.Handle("/api/v1/create_teams", handler.NewCreateTeamsHandler(session, db))
-	mux.Handle("/api/v1/get_books", handler.NewGetBooksHandler(session, db))
+	mux.Handle("/session_test", utils.CORS(handler.NewSessionTestHandler(session)))
+	mux.Handle("/health", utils.CORS(handler.NewHealthHandler()))
+	mux.Handle("/api/v1/login", utils.CORS(handler.NewLoginHandler(session, db)))
+	mux.Handle("/api/v1/register", utils.CORS(handler.NewRegisterHandler(session, db)))
+	mux.Handle("/api/v1/logout", utils.CORS(handler.NewLogoutHandler(session)))
+	mux.Handle("/api/v1/get_teams", utils.CORS(handler.NewGetTeamsHandler(session, db)))
+	mux.Handle("/api/v1/create_teams", utils.CORS(handler.NewCreateTeamsHandler(session, db)))
+	mux.Handle("/api/v1/get_books", utils.CORS(handler.NewGetBooksHandler(session, db)))
 	return mux
 }
---
+++ src/utils/cors.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"net/http"
+)
+
+func CORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,UPDATE,OPTIONS")
+		w.Header().Set("Content-Type", "application/json")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
