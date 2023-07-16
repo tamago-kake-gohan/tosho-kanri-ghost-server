13f58b7cf52baa40600a0caa10faaf3ec76d8c64
xpadev <xpadev@gmail.com>
2023-07-16 12:13:35 +0900

    fix: add isbn to get_book_detail

--- src/handler/get_book_detail.go
+++ src/handler/get_book_detail.go
@@ -13,6 +13,7 @@ type bookDetail struct {
 	OwnerName    string  `json:"owner_name"`
 	BorrowerName string  `json:"borrower_name"`
 	Title        string  `json:"title"`
+	ISBN         string  `json:"isbn"`
 	State        string  `json:"state"` //available, lending, unavailable
 	Rating       float64 `json:"rating"`
 }
@@ -52,14 +53,14 @@ func (h *GetBookDetailHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)
 	userBookId := r.URL.Query().Get("user_book_id")
 	var book bookDetail
 	err := h.db.QueryRow(
-		"SELECT `Owner`.`Name` as `OwnerName`, `Book`.`Title`, `UserBook`.`State`, `Borrower`.`Name` as `BorrowerName`, `Review`.`Rating`"+
+		"SELECT `Owner`.`Name` as `OwnerName`, `Book`.`Title`,`Book`.`ISBN`, `UserBook`.`State`, `Borrower`.`Name` as `BorrowerName`, `Review`.`Rating`"+
 			"FROM `UserBook`"+
 			"INNER JOIN `User` as `Owner` ON `UserBook`.`UserId` = `Owner`.`Id` "+
 			"INNER JOIN `Book` ON `UserBook`.`BookId` = `Book`.`Id`"+
 			"LEFT JOIN `Review` ON `Owner`.`Id` = `Review`.`UserId` AND `Review`.`BookId` = `Book`.`Id`"+
 			"LEFT JOIN `UserLendBook` ON `UserLendBook`.`OwnerId` = `Owner`.`Id` AND `UserLendBook`.`UserBookId` = `UserBook`.`Id` AND `UserLendBook`.`Status` = 'accepted'"+
 			"LEFT JOIN `User` as `Borrower` ON `UserLendBook`.`BorrowerId` = `Borrower`.`Id`"+
-			"WHERE `UserBook`.`Id` = ?", userBookId).Scan(&book.OwnerName, &book.Title, &book.State, &book.BorrowerName, &book.Rating)
+			"WHERE `UserBook`.`Id` = ?", userBookId).Scan(&book.OwnerName, &book.Title, &book.State, &book.BorrowerName, &book.Rating, &book.ISBN)
 	if nil != err {
 		response.Message = "データの取得に失敗しました"
 		response.Status = "error"
